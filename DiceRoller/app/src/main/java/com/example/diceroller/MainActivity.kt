acbaad1fb2fce8a94eefb56263a911660dfb6daf
g-hyoga <hyoga0216@gmail.com>
2020-04-30 13:04:52 +0900

    Next: https://codelabs.developers.google.com/codelabs/kotlin-android-training-images-compat/#5

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,11 +2,12 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    lateinit var diceImage: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,21 +16,33 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
 
+        diceImage = findViewById(R.id.dice_image)
+
+        /*
         val countUpButton: Button = findViewById(R.id.count_up_button)
         countUpButton.setOnClickListener { countUp() }
+         */
     }
 
     private fun rollDice() {
         Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
 
         val randomInt = (1..6).random()
+        val drawableResouce = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
 
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = randomInt.toString()
+        diceImage.setImageResource(drawableResouce)
     }
 
+    /*
     private fun countUp() {
-        val resultText: TextView = findViewById(R.id.result_text)
+        val diceImage: ImageView = findViewById(R.id.dice_image)
         val number = resultText.text.toString().toIntOrNull()
 
         resultText.text = if (number == null) {
@@ -42,4 +55,5 @@ class MainActivity : AppCompatActivity() {
             }.toString()
         }
     }
+     */
 }
