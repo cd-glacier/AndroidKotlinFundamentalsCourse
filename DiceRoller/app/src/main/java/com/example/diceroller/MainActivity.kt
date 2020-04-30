d72a955251f28101f62be7b9a7935951c74c6ed5
g-hyoga <hyoga0216@gmail.com>
2020-04-30 11:49:47 +0900

    Add DiceRoller

---
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.diceroller
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener { rollDice() }
+
+        val countUpButton: Button = findViewById(R.id.count_up_button)
+        countUpButton.setOnClickListener { countUp() }
+    }
+
+    private fun rollDice() {
+        Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+
+        val randomInt = (1..6).random()
+
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = randomInt.toString()
+    }
+
+    private fun countUp() {
+        val resultText: TextView = findViewById(R.id.result_text)
+        val number = resultText.text.toString().toIntOrNull()
+
+        resultText.text = if (number == null) {
+            getText(R.string.default_label)
+        } else {
+            if (number <= 5) {
+                number + 1
+            } else {
+                number
+            }.toString()
+        }
+    }
+}
