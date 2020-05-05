5b8992c427f67a23e30c2c59f4c8ea3b0c82cc9c
g-hyoga <hyoga0216@gmail.com>
2020-05-06 01:53:18 +0900

    https://codelabs.developers.google.com/codelabs/kotlin-android-training-start-external-activity/index.html#4

--- AndroidTrivia/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ AndroidTrivia/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -16,8 +16,12 @@
 
 package com.example.android.navigation
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
@@ -40,6 +44,35 @@ class GameWonFragment : Fragment() {
         val args = GameWonFragmentArgs.fromBundle(arguments!!)
         Toast.makeText(context, "NumCorrect: ${args.numCorrect}, NumQuestions: ${args.numQuestions}", Toast.LENGTH_LONG).show()
 
+        setHasOptionsMenu(true)
+
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.winner_menu, menu)
+
+        if (getShareIntent().resolveActivity(activity!!.packageManager) == null) {
+            menu?.findItem(R.id.share)?.setVisible(false)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item!!.itemId) {
+            R.id.share -> shareSuccess()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun getShareIntent(): Intent {
+        val args = GameWonFragmentArgs.fromBundle(arguments!!)
+        return Intent(Intent.ACTION_SEND)
+            .setType("text/plain")
+            .putExtra(Intent.EXTRA_TEXT, getString(R.string.share_success_text, args.numCorrect, args.numQuestions))
+    }
+
+    private fun shareSuccess() {
+        startActivity(getShareIntent())
     }
 }
