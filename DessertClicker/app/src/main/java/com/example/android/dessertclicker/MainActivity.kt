dd7a31ace17e859d8ef8517af08fb01169e2b20e
g-hyoga <hyoga0216@gmail.com>
2020-05-12 15:20:30 +0900

    https://codelabs.developers.google.com/codelabs/kotlin-android-training-complex-lifecycle/index.html#2

--- DessertClicker/app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
+++ DessertClicker/app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
@@ -18,7 +18,6 @@ package com.example.android.dessertclicker
 
 import android.content.ActivityNotFoundException
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
@@ -32,6 +31,8 @@ class MainActivity : AppCompatActivity() {
 
     private var revenue = 0
     private var dessertsSold = 0
+
+    private lateinit var dessertTimer: DessertTimer
 
     // Contains all the views
     private lateinit var binding: ActivityMainBinding
@@ -75,6 +76,8 @@ class MainActivity : AppCompatActivity() {
             onDessertClicked()
         }
 
+        dessertTimer = DessertTimer()
+
         // Set the TextViews to the right values
         binding.revenue = revenue
         binding.amountSold = dessertsSold
@@ -85,6 +88,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
+        dessertTimer.startTimer()
 
         Timber.i("onStart Called")
     }
@@ -101,6 +105,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStop() {
         super.onStop()
+        dessertTimer.stopTimer()
+
         Timber.i("onStop Called")
     }
 
