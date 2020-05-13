1f9ed0f32fff4be91a8cd7b9f5748e124b343523
g-hyoga <hyoga0216@gmail.com>
2020-05-13 11:11:21 +0900

    https://codelabs.developers.google.com/codelabs/kotlin-android-training-view-model/#6

--- GuessTheWord/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ GuessTheWord/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -21,9 +21,11 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.NavHostFragment
 import com.example.android.guesstheword.R
 import com.example.android.guesstheword.databinding.GameFragmentBinding
 
@@ -51,6 +53,7 @@ class GameFragment : Fragment() {
 
         binding.correctButton.setOnClickListener { onCorrect() }
         binding.skipButton.setOnClickListener { onSkip() }
+        binding.endGameButton.setOnClickListener { onEndGame() }
         updateScoreText()
         updateWordText()
         return binding.root
@@ -70,6 +73,10 @@ class GameFragment : Fragment() {
         updateScoreText()
     }
 
+    private fun onEndGame() {
+        gameFinished()
+    }
+
     /** Methods for updating the UI **/
 
     private fun updateWordText() {
@@ -78,5 +85,12 @@ class GameFragment : Fragment() {
 
     private fun updateScoreText() {
         binding.scoreText.text = viewModel.score.toString()
+    }
+
+    private fun gameFinished() {
+        Toast.makeText(activity, "Game has just finished", Toast.LENGTH_SHORT).show()
+        val action = GameFragmentDirections.actionGameToScore()
+        action.score = viewModel.score
+        NavHostFragment.findNavController(this).navigate(action)
     }
 }
--- GuessTheWord/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ GuessTheWord/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.android.guesstheword.screens.game
 
 import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
