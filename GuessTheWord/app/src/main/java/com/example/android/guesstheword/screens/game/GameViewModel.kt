40ef74dae8d705ce888b7daf309989daa34d9397
g-hyoga <hyoga0216@gmail.com>
2020-05-16 11:22:24 +0900

    https://codelabs.developers.google.com/codelabs/kotlin-android-training-live-data-transformations/#4

--- GuessTheWord/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ GuessTheWord/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,10 +1,12 @@
 package com.example.android.guesstheword.screens.game
 
 import android.os.CountDownTimer
+import android.text.format.DateUtils
 import android.util.Log
 import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
@@ -31,6 +33,10 @@ class GameViewModel : ViewModel() {
     private val _currentTime = MutableLiveData<Long>()
     val currentTime: LiveData<Long>
         get() = _currentTime
+    // The String version of the current time
+    val currentTimeString = Transformations.map(currentTime) { time ->
+        DateUtils.formatElapsedTime(time)
+    }
 
     private val timer: CountDownTimer
 
