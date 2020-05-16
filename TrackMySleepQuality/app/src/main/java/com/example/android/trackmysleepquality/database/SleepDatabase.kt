ee902128eb7d9d5ce0860c65907b48fb05a2f51f
g-hyoga <hyoga0216@gmail.com>
2020-05-16 12:53:14 +0900

    https://codelabs.developers.google.com/codelabs/kotlin-android-training-room-database/#5

--- TrackMySleepQuality/app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
+++ TrackMySleepQuality/app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
@@ -16,9 +16,13 @@
 
 package com.example.android.trackmysleepquality.database
 
+import android.content.Context
 import androidx.room.ColumnInfo
+import androidx.room.Database
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.Room
+import androidx.room.RoomDatabase
 
 @Entity(tableName = "daily_sleep_quality_table")
 data class SleepNight(
@@ -34,3 +38,31 @@ data class SleepNight(
     @ColumnInfo(name = "sleep_quality")
     var sleepQuality: Int = -1
 )
+
+@Database(entities = [SleepNight::class], version = 1, exportSchema = false)
+abstract class SleepDatabase : RoomDatabase() {
+    abstract val sleepDatabaseDao: SleepDatabaseDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: SleepDatabase? = null
+
+        fun getInstance(context: Context): SleepDatabase {
+            synchronized(this) {
+                var instance = INSTANCE
+
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        SleepDatabase::class.java,
+                        "sleep_history_database")
+                        .fallbackToDestructiveMigration()
+                        .build()
+                    INSTANCE = instance
+                }
+
+                return instance
+            }
+        }
+    }
+}
