58d1d23dc5c60e4742ca2220a01838c50c3f8875
g-hyoga <hyoga0216@gmail.com>
2020-05-19 00:26:25 +0900

    https://codelabs.developers.google.com/codelabs/kotlin-android-training-recyclerview-fundamentals/#4

--- TrackMySleepQuality/app/src/main/java/com/example/android/trackmysleepquality/Util.kt
+++ TrackMySleepQuality/app/src/main/java/com/example/android/trackmysleepquality/Util.kt
@@ -26,6 +26,8 @@ import androidx.core.text.HtmlCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android.trackmysleepquality.database.SleepNight
 import java.text.SimpleDateFormat
+import java.util.Locale
+import java.util.concurrent.TimeUnit
 
 /**
  * These functions create a formatted string that can be set in a TextView.
@@ -107,3 +109,24 @@ fun formatNights(nights: List<SleepNight>, resources: Resources): Spanned {
 
 class TextItemViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
 
+private val ONE_MINUTE_MILLIS = TimeUnit.MILLISECONDS.convert(1, TimeUnit.MINUTES)
+private val ONE_HOUR_MILLIS = TimeUnit.MILLISECONDS.convert(1, TimeUnit.HOURS)
+
+fun convertDurationToFormatted(startTimeMilli: Long, endTimeMilli: Long, res: Resources): String {
+    val durationMilli = endTimeMilli - startTimeMilli
+    val weekdayString = SimpleDateFormat("EEEE", Locale.getDefault()).format(startTimeMilli)
+    return when {
+        durationMilli < ONE_MINUTE_MILLIS -> {
+            val seconds = TimeUnit.SECONDS.convert(durationMilli, TimeUnit.MILLISECONDS)
+            res.getString(R.string.seconds_length, seconds, weekdayString)
+        }
+        durationMilli < ONE_HOUR_MILLIS -> {
+            val minutes = TimeUnit.MINUTES.convert(durationMilli, TimeUnit.MILLISECONDS)
+            res.getString(R.string.minutes_length, minutes, weekdayString)
+        }
+        else -> {
+            val hours = TimeUnit.HOURS.convert(durationMilli, TimeUnit.MILLISECONDS)
+            res.getString(R.string.hours_length, hours, weekdayString)
+        }
+    }
+}
