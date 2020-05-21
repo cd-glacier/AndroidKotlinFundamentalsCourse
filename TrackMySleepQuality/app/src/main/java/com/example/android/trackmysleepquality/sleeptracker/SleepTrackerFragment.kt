748c7a0eb0a603a90445d13f2d422bb4e4789045
g-hyoga <hyoga0216@gmail.com>
2020-05-21 10:02:24 +0900

    https://codelabs.developers.google.com/codelabs/kotlin-android-training-grid-layout/#3

--- TrackMySleepQuality/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ TrackMySleepQuality/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -25,6 +25,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.android.trackmysleepquality.R
 import com.example.android.trackmysleepquality.database.SleepDatabase
 import com.example.android.trackmysleepquality.databinding.FragmentSleepTrackerBinding
@@ -86,6 +87,9 @@ class SleepTrackerFragment : Fragment() {
                 adapter.submitList(it)
             }
         })
+
+        var manager = GridLayoutManager(activity, 3)
+        binding.sleepList.layoutManager = manager
 
         return binding.root
     }
